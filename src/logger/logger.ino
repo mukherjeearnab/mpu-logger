fdbc6acce2f003188182f5a44bc69d985558ebe1
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-05-08 16:15:18 +0530

    Added Complete functionality of logger

--- src/logger/logger.ino
+++ src/logger/logger.ino
@@ -1,11 +1,55 @@
+// WRITE ENABLE PIN number
+const int WRITE_ENABLE_PIN = 7;
+
+// Reading and writing globals
+float readings[6];
+char csvString[100];
+
+// Setup Method
 void setup() {
     // Set Activity Pin
     pinMode(LED_BUILTIN, OUTPUT);
+
+    // Set Write Enable Pin
+    pinMode(LED_BUILTIN, INPUT);
+
+    // Initialize SD Card
+    initSDCard();
+
+    // Initialize MPU6050
+    initMPU6050();
+
+    // Write CSV Headers to SD Card
+    write2SDCard("test.csv", "timestamp,ax,ay,az,gx,gy,gz\n");
 }
 
+// LOOP method
 void loop() {
+    // Fetch MPU6050 Reading
+    getMPU6050Reading();
+
+    // Generate CSV String for the Readings
+    getCSVOutput(readings);
+
+    // Log CSV Output to console
+    Serial.print(csvString);
+
+    // If Writing is enabled, write to the CSV
+    if (digitalRead(WRITE_ENABLE_PIN) == HIGH)
+        write2SDCard("test.csv", csvString);
+    else
+        Serial.print("Writing to SD card Disabled!");
+
+    // Sleep for 25ms
+    delay(25);
 }
 
 // -------------------
 // Auxiliary functions
 // -------------------
+
+// Generate MPU6050 readings as CSV String
+void getCSVOutput(float* readings) {
+    // fstring '%d,%02f,%02f,%02f,%02f,%02f,%02f\n'
+    sprintf(csvString, "%d,%02f,%02f,%02f,%02f,%02f,%02f\n", micros(), readings[0], readings[1], readings[2], readings[3], readings[4], readings[5]);
+}
--- src/logger/mpu6050.ino
+++ src/logger/mpu6050.ino
@@ -30,3 +30,18 @@ void initMPU6050() {
 
     Serial.println("MPU6050 initialization done.");
 }
+
+// Get MPU6050 readings
+void getMPU6050Reading() {
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    // Populate Sensor Reading
+    readings[0] = a.acceleration.x;
+    readings[1] = a.acceleration.y;
+    readings[2] = a.acceleration.z;
+    readings[3] = g.gyro.x;
+    readings[4] = g.gyro.y;
+    readings[5] = g.gyro.z;
+}
