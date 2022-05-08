ad5fab359137b3e09219ec5b14908bdf40cb7cd6
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-05-08 14:02:08 +0530

    Added SD Card Read Test

---
+++ tests/sdcard-read/sdcard-read.ino
@@ -0,0 +1,42 @@
+#include <SD.h>
+#include <SPI.h>
+
+File myFile;
+
+void setup() {
+    // Open serial communications and wait for port to open:
+    Serial.begin(115200);
+    while (!Serial) {
+        ;  // wait for serial port to connect. Needed for native USB port only
+    }
+
+    // Initialize SD Card
+    Serial.print("Initializing SD card...");
+    if (!SD.begin(10)) {
+        Serial.println("initialization failed!");
+        while (1)
+            ;
+    }
+    Serial.println("initialization done.");
+
+    // open the file for reading:
+    myFile = SD.open("test.txt");
+    if (myFile) {
+        Serial.println("test.txt:");
+
+        // read from the file until there's nothing else in it:
+        while (myFile.available()) {
+            Serial.write(myFile.read());
+        }
+
+        // close the file:
+        myFile.close();
+    } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+}
+
+void loop() {
+    // nothing happens after setup
+}
